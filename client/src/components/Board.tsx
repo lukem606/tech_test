7d669b8e34d1ac6c83ddd87aa9b8e0bfb3221881
Luke Martin <luke_martin93@hotmail.co.uk>
2026-02-19 23:38:30 +0000

    Add type to Square and Board components

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import { XorO } from "../types";
 import { Square } from "./Square";
 
@@ -7,7 +7,7 @@ interface BoardProps {
     onClickSquare: (rowIndex: number, columnIndex: number) => void;
 }
 
-export const Board = ({ board, onClickSquare }: BoardProps) => {
+export const Board: FC<BoardProps> = ({ board, onClickSquare }: BoardProps) => {
     return <div className='flex flex-col gap-1'>
         {board.map((row, rowIndex) => <div className='flex gap-1'>
             {row.map((column, columnIndex) => <Square key={`square-${rowIndex}-${columnIndex}`} value={column} onClickSquare={() => onClickSquare(rowIndex, columnIndex)}></Square>)}
