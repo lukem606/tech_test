885730c0a839edeb6694178de58439f78891041d
Luke Martin <luke_martin93@hotmail.co.uk>
2026-02-19 19:24:17 +0000

    Add logic to change board state on square click

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Board } from './components/Board';
 import { XorO } from './types';
+import { CurrentPlayer } from './enums';
 
 
 export const Main = () => {
@@ -10,8 +11,19 @@ export const Main = () => {
     [undefined, undefined, undefined]
   ])
 
+  const [currentPlayer, setCurrentPlayer] = useState<XorO>(CurrentPlayer.X);
+
+  const handleClickSquare = (rowIndex: number, columnIndex: number): void => {
+    const newBoard = [...board];
+    newBoard[rowIndex][columnIndex] = currentPlayer;
+
+    setBoard(newBoard);
+    setCurrentPlayer(currentPlayer === CurrentPlayer.X ? CurrentPlayer.O : CurrentPlayer.X
+    )
+  }
+
   return <div className='flex flex-col mt-10 items-center gap-10'>
     <div className='font-bold text-2xl'>Tic Tac Toe</div>
-    <Board board={board}></Board>
+    <Board board={board} onClickSquare={handleClickSquare}></Board>
   </div>
 }
