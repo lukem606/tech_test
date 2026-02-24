fee7d445a13ccd78c29b3178e771f1cb41a8de40
Luke Martin <luke.martin@iowna.com>
2026-02-24 16:29:23 +0000

    Update database from linting

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -2,11 +2,14 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(
+  process.env.DATABASE_URL ?? "postgres://user:password@db:5432/database",
+);
 
 const runMigrations = async () => {
-  console.log("Running migrations");
   await migrate(db, { migrationsFolder: "./drizzle" });
 };
 
-runMigrations();
+runMigrations().catch((error: unknown) => {
+  console.error("An error occurred while running database migrations", error);
+});
